4649f9eba7b94d711c29732a4d5387daa3fe51ab
Omji Kushwaha <omjimaurya09@gmail.com>
2025-06-16 19:50:21 +0530

    feat:added dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
-
+import { config } from "dotenv";
 const app = express();
 const PORT = process.env.PORT || 8000;
 import contactRouter from "./contacts/contact.route";
+
+config({ path: ".env" });
+app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
 });
 
-app.use(express.json());
 app.use("/identity", contactRouter);
 
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
